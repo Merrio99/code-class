c05fa0143d735ef51a88d2f3aa543a6ffd2eb92b
Merrio99 <soplamocos3@gmail.com>
2022-03-30 12:41:44 -0600

    actualizacion de proyecto

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -9,6 +9,16 @@ export class CardComponent implements OnInit {
   @Input() employeeID: number = 1;
   @Input() employeeName: string = "";
   @Input() gender: string ="";
+  @Input() hireDate: string ="";
+  @Input() jobTitle: string ="";
+  @Input() loginID: string ="";
+  @Input() maritalStatus: string ="";
+  @Input() modifiedDate: string ="";
+  @Input() nationalIDNumber: string ="";
+  @Input() rowGuide: string ="";
+  @Input() sickLeaveHours: string ="";
+  @Input() vacationHours: string ="";
+  @Input() urlImage: string = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQR71NMInPLzHQXuT-nuGGPOS5g5GTD9Iwxvw&usqp=CAU";
   constructor() { }
   ngOnInit(): void {
   }
